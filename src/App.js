858f2eb35faf6999f3d2b8c93a48a2439e4e54c9
Matthew Conroy <mmconroy@gmail.com>
2020-05-18 11:07:12 -0400

    Rendering Activity

--- src/App.js
+++ src/App.js
@@ -2,37 +2,61 @@ import React from "react";
 import "./App.css";
 import avatar from "./img/avatar.svg";
 
+const todoList = [
+  {
+    id: 1,
+    title: "item title",
+    description: "your description",
+    completed: false,
+  },
+  {
+    id: 2,
+    title: "item title",
+    description: "your description",
+    completed: false,
+  },
+  {
+    id: 3,
+    title: "item title",
+    description: "your description",
+    completed: false,
+  },
+];
+
+const user = {
+  name: "William",
+  avatarURL: "http://www.fillmurray.com/100/100",
+};
+
 function App() {
   return (
-    <div className="title">
-      <h1>Todo.ly</h1>
+    <div className="body">
       <header className="header">
-        <img src={avatar} alt="Avatar"></img>
-        <h2>Username</h2>
+        <h1 className="title">Todoly</h1>
+        <section classname="user__container">
+          <h1>{user.name}</h1>
+          <img src={user.avatarURL} />
+        </section>
+        <nav className="sidenav">
+          <a href="#">Nav 1</a>
+          <a href="#">Nav 2</a>
+          <a href="#">Nav 3</a>
+          <a href="#">Nav 4</a>
+          <a href="#">Nav 5</a>
+          <a href="#">Nav 6</a>
+          <a href="#">Settings</a>
+        </nav>
       </header>
-      <nav className="sidenav">
-        <a href="#">Nav 1</a>
-        <a href="#">Nav 2</a>
-        <a href="#">Nav 3</a>
-        <a href="#">Nav 4</a>
-        <a href="#">Nav 5</a>
-        <a href="#">Nav 6</a>
-        <a href="#">Settings</a>
-      </nav>
       <main className="todo-list">
         <h1 className="your-tasks">Your Tasks</h1>
-        <input type="text" placeholder="Search Here"></input>
-        <input type="checkbox" id="task1" name="task1" value="FirstTask" />
-        <label for="task1"> First Todo Item</label>
-        <input type="checkbox" id="task2" name="task2" value="SecondTask" />
-        <label for="task1"> Second Todo Item</label>
-        <input type="checkbox" id="task3" name="task3" value="ThirdTask" />
-        <label for="task1"> Third Todo Item</label>
-        <input type="checkbox" id="task4" name="task4" value="FourthTask" />
-        <label for="task1"> Fourth Todo Item</label>
-        <input type="checkbox" id="task5" name="task5" value="FifthTask" />
-        <label for="task1"> Fifth Todo Item</label>
-        <button type="button">Add a New Task</button>
+        <ul>
+          {todoList.map((todo) => (
+            <li>
+              <p>{todo.title}</p>
+              <p>{todo.description}</p>
+            </li>
+          ))}
+        </ul>
       </main>
 
       <section className="task-details">
